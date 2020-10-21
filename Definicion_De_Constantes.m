2d1705b24584361b85743e30072b77357ac8e06d
Sebastian <smilhas@itba.edu.ar>
2020-10-21 01:44:04 -0300

    Constantes de ejemplo y dejo para que funcione

---
+++ Definicion_De_Constantes.m
@@ -0,0 +1,13 @@
+%% Definicion de constantes (no funcionan)
+m=10; M=m*0.7;
+b=4; B=b*0.85;
+k=5; K=k*1.20;
+Kp=-4.6*1;
+Kv=0.5*Kp*0;
+
+%% Las que tenía Tomi en la clase (funcionan)
+m=10; M=m*0.7;
+b=4; B=b*0.85;
+k=5; K=k*2;
+Kp=5;
+Kv=2*sqrt(Kp);
