58fb9550543732455f6936232c06b5a0087f3059
Sebastian <smilhas@itba.edu.ar>
2020-11-05 02:50:21 -0300

    Dejo el ej 01 funcionando

--- MATLAB 2018/Ej_01.m
+++ MATLAB 2018/Ej_01.m
@@ -1,21 +1,18 @@
-%% CreaciÃ³n del brazo RR
+%% Creacion del brazo RR
 
 N = 2; % cantidad de links
+
+% Largos 1m
 L1 = 1;
 L2 = 1;
 
 %Perturbacion
 pert = 0.1;
-% Links de revolucion              'd', 0
-d=0;
-
-% Largos 1m
-l1 = 1;
-l2 = 1;
 
 % Masa 1kg
 m = 1 +(2*rand()-1)*pert;
-% Centro de masa en extremo:  'r', [1 0 0]
+
+% Centro de masa en extremo
 r=1;
 rv = [r+(2*rand()-1)*pert, (2*rand()-1)*pert, 0];
 
@@ -35,42 +32,73 @@ for  iLink = 1:N
             DH(iLink).alpha, 'm', m, 'r', rv, 'B', b, 'modified'); % Vector de estructuras Link
 end
 
-Tool = transl([L2, 0, 0]); % Offset de la herramienta
+Tool = transl(L2, 0, 0); % Offset de la herramienta
 
 messi = SerialLink([links{:}], 'tool', Tool, 'name', 'messi');
 
-%% Calculo de trayectoria
-q0=[0 -pi/2]; %eligo un angulo inicial
+%% CALCULO DE TRAYECTORIAS
+q0=[0 -pi/2]; %Elijo un angulo inicial
 
 T1=transl(1,-1,0);
 T2=transl(1,1,0);
 
 q1=messi.ikine(T1,'q0', q0, 'mask', [1, 1, 0, 0, 0, 0]);                    %Posicion joint inicial
 q2=messi.ikine(T2,'q0', q0, 'mask', [1, 1, 0, 0, 0, 0]);                    %Posicion joint final
-t=[0:0.05:2]';                          %Tiempo de trayectoria propuesto
 
-% TRAYECTORIAS EN ESPACIO DE ARTICULACION
+% Definición del tiempo
+step = 0.05;
+Tmax = 2;
+t=(0:step:Tmax)';                          %Tiempo de trayectoria propuesto
+
+% Trayectorias en espacio joint
 %Aplica trayectoria con polinomio de grado 5, con velocidad inicial y final igual a 0
 [q,qd,qdd]=jtraj(q1,q2,t);
 
-p1 = [1 -1 0];
-pm = [1 0 0];
-p2 = [1 1 0];
+% Calculo trayectoria en caratesianas a partir del espacio joint
+T = messi.fkine(q);
+x_j=transl(T);
+x_j = x_j(:,[1,2]);
+figure(1)
+plot(t,x_j)
+grid on
+title('Trayectoria del EE en cartesianas (generada en espacio joint)')
+legend('x','y','z')
 
+% Sino mas facil calculo la trayectoria en cartesianas directamente
+Tcart=ctraj(T1,T2,length(t));
+x_c=transl(Tcart);
+x_c = x_c(:,[1,2]);
+figure(2)
+plot(t,x_c)
+title('Trayectoria cartesiana')
+xlabel('Tiempo');
+ylabel('Posicion');
+grid on
+legend('x','y','z')
 
-points = [p1; p2];
-times = [0, 1.1];
-step_t = 1e-2; %step de trayectoria
+% velocidad en cartesianas
+xd_j = [diff(x_j)/step; 0 0];
+figure(3)
+plot(t,xd_j)
+title('Velocidad cartesiana')
+xlabel('Tiempo');
+ylabel('Velocidad');
+grid on
+legend('xd','yd','zd')
 
+% aceleración en cartesianas
+xdd_j = [diff(xd_j)/step; 0 0];
+figure(4)
+plot(t,xdd_j)
+title('Aceleración cartesiana')
+xlabel('Tiempo');
+ylabel('Aceleración');
+grid on
+legend('xdd','ydd','zdd')
 
-[X, Xd, Xdd] = genTraj(points, times, step_t);
-t = 0: step_t:max(times);
-Xt = timeseries (X,t);
-Xdt = timeseries (Xd,t);
-Xddt = timeseries (Xdd,t);
-
-%Graficos de Trayectoria en espacio de Joint
-messi.plot(q);
+Xt = timeseries (x_j,t);
+Xdt = timeseries (xd_j,t);
+Xddt = timeseries (xdd_j,t);
 
 %% Prueba del toolbox
 
