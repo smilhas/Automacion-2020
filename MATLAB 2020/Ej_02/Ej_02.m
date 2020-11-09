57c8bd62e343b139c06fc7946748f8d7dd2b7482
mati <mpierdominici@itba.edu.ar>
2020-11-09 14:55:58 -0300

    el ej 2 piolas piolasa con el 1000 magico

--- MATLAB 2020/Ej_02/Ej_02.m
+++ MATLAB 2020/Ej_02/Ej_02.m
@@ -1,64 +1,74 @@
-%% Creación del brazo RR
-
-N = 2; % cantidad de links
-L1 = 1;
-L2 = 1;
+%%                       TP 01 - Ej 01
+close all
+clc
+%% Creacion del brazo RR
 
 %Perturbacion
 pert = 0.1;
-% Links de revolucion              'd', 0
-d=0;
 
-% Largos 1m
-l1 = 1;
-l2 = 1;
+messi = RobotInit(pert);
 
-% Masa 1kg
-m = 1 +(2*rand()-1)*pert;
-% Centro de masa en extremo:  'r', [1 0 0]
-r=1;
-rv = [r+(2*rand()-1)*pert, (2*rand()-1)*pert, 0];
+%% CALCULO DE TRAYECTORIAS
+% Transformadas homogeneas inicial/final
+T1=transl(1.9,0.100015,0); % Con el mil
+%T1=transl(1.9,0.115,0); % Sin el mil
+T2=transl(0.1,1.9,0);
 
-% Friccion unitaria:     'B', 1
-b = 1;  
+% C�lculo de posicion inicial/final en espacio joint
+%q0=[0 -pi/2]; %Elijo un angulo inicial
+q0 = [-0.260729892263819   0.626641653599234]; % Este empoieza con 10.6 N (con el mil)
+%q0 = [-0.251492072201899   0.623889277245919]; % Este empoieza con 10.6 N (sin el mil)
+%q0 = [-0.260732942908050   0.626642506273637];  % Este empieza con 7 N
+%q0= [-20.53*pi/180  pi/4+abs(-20.53*pi/180)];
+%T1 = messi.fkine(q0);
 
-% Planar, sin momento de inercia.
-g=9.81;
+q1=messi.ikine(T1,'q0', q0, 'mask', [1, 1, 0, 0, 0, 0]);    %Posicion joint inicial
+q2=messi.ikine(T2,'q0', q0, 'mask', [1, 1, 0, 0, 0, 0]);    %Posicion joint final
 
-DH = struct('d', cell(1,N), 'a', cell(1,N), 'alpha', cell(1,N), 'theta', cell(1,N),...
-    'type', cell(1,N)); %genera estructura base
-DH(1).alpha = 0;    DH(1).a = 0;    DH(1).d = 0;    DH(1).type = 'R';
-DH(2).alpha = 0;    DH(2).a = L1;   DH(2).d = 0;    DH(2).type = 'R';
+% Definici�n del tiempo
+step = 0.05;
+Tmax = 4;
+t=(0:step:Tmax)';   %Tiempo de trayectoria propuesto
 
-for  iLink = 1:N
-        links{iLink} = Link('d', DH(iLink).d, 'a', DH(iLink).a, 'alpha', ...
-            DH(iLink).alpha, 'm', m, 'r', rv, 'B', b, 'modified'); % Vector de estructuras Link
+% Trayectorias en espacio joint
+[q,qd,qdd]=jtraj(q0,q2,t);
+PlotTraj(t,q,'Trayectoria en espacio joint',['Tiempo', 'Angulo'], ['tita_1','tita_2'], 1);
+
+% Calculo trayectoria en caratesianas a partir del espacio joint
+T = messi.fkine(q);
+x_j=transl(T);
+% posici�n en cartesianas
+x_j = x_j(:,[1,2]);
+% velocidad en cartesianas
+xd_j = [diff(x_j)/step; 0 0];
+% aceleraci�n en cartesianas
+xdd_j = [diff(xd_j)/step; 0 0];
+PlotTraj(t,x_j,'Trayectoria del EE en cartesianas (generada en espacio joint)',['Tiempo', 'Posicion'], ['x','y'], 3);
+
+% Sino mas facil calculo la trayectoria en cartesianas directamente
+Tcart=ctraj(T1,T2,length(t));
+x_c=transl(Tcart);
+x_c = x_c(:,[1,2]);
+xd_c = [diff(x_c)/step; 0 0];
+xdd_c = [diff(xd_c)/step; 0 0];
+PlotTraj(t,x_c,'Trayectoria cartesiana',['Tiempo', 'Posicion'], ['x','y'], 4);
+
+Xt = timeseries (x_j,t);
+Xdt = timeseries (xd_j,t);
+Xddt = timeseries (xdd_j,t);
+
+Xt = timeseries (x_c,t);
+Xdt = timeseries (xd_c,t);
+Xddt = timeseries (xdd_c,t);
+
+%% Definiciones de funci�n
+
+function PlotTraj(t,x,plotTitle,labels,legends,index)
+    figure(index)
+    plot(t,x)
+    title(plotTitle)
+%     xlabel(labels(1));
+%     ylabel(labels(2));
+    grid on
+%     legend(legends(1),legends(2))
 end
-
-Tool = transl([L2, 0, 0]); % Offset de la herramienta
-
-messi = SerialLink([links{:}], 'tool', Tool, 'name', 'messi');
-
-%% Calculo de trayectoria
-q0=[0 0]; %eligo un angulo inicial
-
-T1=transl(1,-1,0);
-T2=transl(1,1,0);
-
-q1=messi.ikine(T1,'q0', [pi/2,pi/2], 'mask', [1, 1, 0, 0, 0, 0]);                    %Posicion joint inicial
-q2=messi.ikine(T2,'q0', [pi/2,pi/2], 'mask', [1, 1, 0, 0, 0, 0]);                    %Posicion joint final
-t=[0:0.05:2]';                          %Tiempo de trayectoria propuesto
-
-% TRAYECTORIAS EN ESPACIO DE ARTICULACION
-%Aplica trayectoria con polinomio de grado 5, con velocidad inicial y final igual a 0
-[q,qd,qdd]=jtraj(q1,q2,t);
-
-%Graficos de Trayectoria en espacio de Joint
-messi.plot(q);
-
-%% Prueba del toolbox
-
-messi.rne([q,qd,qdd])
-%% 
-POLY = [0 2 0; 2 0 0];
-H = plot_poly(POLY, 'animate', 'b'); % draw a red polygo
